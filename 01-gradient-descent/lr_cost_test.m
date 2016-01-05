d576f4163ea1fd05b7a4226177eef83f1d547497
Iurii Kyian <iurii.kyian@insights.md>
2016-01-05 14:02:05 +0100

    added optional regularization parameter for linear regression cost function

--- 01-gradient-descent/lr_cost_test.m
+++ 01-gradient-descent/lr_cost_test.m
@@ -11,3 +11,6 @@ fprintf("ideal theta cost: %d\n", c0);
 Theta = [1, 2]
 c1 = lr_cost(Theta, X, Y);
 fprintf("NOT ideal theta cost: %d\n", c1);
+lambda = 10;
+c2 = lr_cost(Theta, X, Y, lambda);
+fprintf("NOT ideal theta with regularization lambda: %d cost: %d\n", lambda, c2);
--- lib/lr_cost.m
+++ lib/lr_cost.m
@@ -1,11 +1,21 @@
-function J = lr_cost(theta, X, Y)
+function J = lr_cost(theta, X, Y, lambda)
   % Cost for linear regression
   %
   % theta - 1xN matrix with theta values
   % X     - Mx(N-1) matrix of data
   % Y     - Nx1 matrix of values
-  % derivation dgree of calculated Y from provided Y
+  % lambda - optional regularization parameter 
+  %
+  % Returns:
+  %     derivation dgree of calculated Y from provided Y
   m = size(X)(1);
   XX = [ones(m, 1), X];
   J = sum(((XX * theta')  - Y) .^2) / (2 * m);
+
+  if nargin == 4
+    t = theta;
+    t(1,1) = 0;
+    r = (lambda / ( 2 * m)) * sum( t .^ 2)
+    J = J + r;
+  end;  
 end;
